061d09874d831a00b20ea719a379c83068106ced
isiakaabd <56041462+isiakaabd@users.noreply.github.com>
2022-09-04 22:22:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-u1pzxx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-u1pzxx)
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+import './App.css';
+import { Button ,Container,Grid} from '@mui/material';
+
+function App() {
+  const [count, setCount] = useState(10);
+
+  return (
+    <Container sx={{background:"red"}}>
+    sadfsdsds
+    </Container>
+  );
+}
+
+export default App;
